4a93f180d51a6c002280abbf767c5aef37e374ab
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-03-09 00:14:24 -0300

    Desafio 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { connect } from 'react-redux';
 import ExpensiveForms from '../components/ExpenseForms';
 import Header from '../components/Header';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <ExpensiveForms />
+        <Table />
       </div>
     );
   }
