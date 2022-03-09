02dc3c91fd9f90263f873f34341e94983f102b8c
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-03-09 11:09:24 -0300

    Desafio 7

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Proptypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
-    return ( // Aqui só irei montar o cabeçalho, como o requisito pede.
+    const { expenses } = this.props;
+    return (
       <table>
         <thead>
           <tr>
@@ -17,9 +20,40 @@ class Table extends React.Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          { expenses.map(({
+            description,
+            tag,
+            method,
+            value,
+            exchangeRates,
+            currency,
+            id }) => {
+            const extendNameCoin = exchangeRates[currency].name; // queria por tudo dentro da tag só q o lint n deixou. Tive q criar essa variável.
+            return (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{ extendNameCoin }</td>
+                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>{(value * exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>Real</td>
+              </tr>); // sem o toFixed as casas decimais lascam o código.
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: Proptypes.object,
+}.isRequired;
+
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
